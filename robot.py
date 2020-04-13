4f2afa62d49b5651b752f10b7b1ac2178bb0a475
ypliu <resodo.liu@gmail.com>
2020-04-13 22:51:47 +0800

    improve

--- robot.py
+++ robot.py
@@ -15,6 +15,8 @@ time_interval = int(config['time_interval'])
 
 bad_request_count = 0
 bad_request_bar = INITIAL_BAD_REQUEST_BAR
+last_status = 0
+last_msg = ""
 tl = Timeloop()
 
 
@@ -35,20 +37,33 @@ def make_payload(msg, at_all=False):
 def monitor_job():
     global bad_request_count
     global bad_request_bar
+    global last_status
+    global last_msg
+
     try:
         monitor('miner_list.txt')
         bad_request_count = 0
         bad_request_bar = INITIAL_BAD_REQUEST_BAR
+        if last_status != 0:
+            requests.post(DINGTALK_URL.format(dingtalk_token),
+                          json=make_payload("all miners are back to work now", at_all=True))
+        last_status = 0
+
     except MiningError as err:
         bad_request_count = 0
         bad_request_bar = INITIAL_BAD_REQUEST_BAR
-        requests.post(DINGTALK_URL.format(dingtalk_token), json=make_payload(err.message))
+        if last_status == 0 or last_status == 1 and last_msg != err.message:
+            requests.post(DINGTALK_URL.format(dingtalk_token), json=make_payload(err.message, at_all=True))
+            last_msg = err.message
+        last_status = 1
+
     except RequestError:
         bad_request_count += 1
         if bad_request_count >= bad_request_bar:
             requests.post(DINGTALK_URL.format(dingtalk_token),
                           json=make_payload("bad request on ConfluxScan API for {} times.".format(bad_request_count)))
             bad_request_bar *= 2
+
     except Exception as e:
         print('Unexpected error: ', e)
         requests.post(DINGTALK_URL.format(dingtalk_token), json=make_payload("Unexpected error: {}.".format(str(e))))
